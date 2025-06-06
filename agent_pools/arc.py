269788d5c362f85fe76332fecf9728980edaba11
Arnav-Gr0ver <arnavgrover@gmail.com>
2025-06-06 19:54:10 -0400

    stash
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

---
+++ agent_pools/arc.py
@@ -0,0 +1,25 @@
+
+
+class Agent:
+    def __init__(self, api_key, model, role, state, tools, memory, message_history):
+        self.api_key = api_key
+        self.model = model
+        self.role = role
+        self.state = state
+        self.tools = tools
+        self.memory = memory
+        self.message_history = message_history
+    def init_agent(self):
+        #TODO: add agent creation func here
+        print("hello")
+    def run(self):
+        #TODO: add run agent func here
+        print("hello")
+
+class AgentPool:
+    def __init__(self):
+        #agents
+        print("hello")
+    def run(self):
+        #swarm query
+        print("hello")
